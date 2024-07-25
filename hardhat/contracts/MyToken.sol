dc4140760a0b141924647c8eca7b400cf59f00ab
blinklabs-admin <mike@blinklabs.ai>
2024-07-25 23:23:35 +0800

    Create an ERC20 standard token contract with the following features: Multisend that inherits from OpenZeppelin Multicall, Gasless transaction that inherits from OpenZeppelin ERC20Permit, Burn that inherits from OpenZeppelin ERC20Burnable, and Pausable that inherits from OpenZeppelin ERC20Pausable.

--- hardhat/contracts/MyToken.sol
+++ hardhat/contracts/MyToken.sol
@@ -1,9 +1,78 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/utils/Multicall.sol";
 
-contract MyToken is ERC20Capped {
-    constructor(string memory _name, string memory _symbol, uint256 _maxSupply) ERC20(_name, _symbol) ERC20Capped(_maxSupply) {}
+contract MyToken is ERC20, ERC20Burnable, ERC20Pausable, ERC20Permit, Multicall {
+    uint256 private immutable _maxSupply;
+
+    /**
+     * @dev Constructor that sets the name, symbol, and max supply of the token.
+     * @param name_ The name of the token.
+     * @param symbol_ The symbol of the token.
+     * @param maxSupply_ The maximum supply of the token.
+     */
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 maxSupply_
+    ) ERC20(name_, symbol_) ERC20Permit(name_) {
+        _maxSupply = maxSupply_;
+        _mint(msg.sender, maxSupply_);
+    }
+
+    /**
+     * @dev Pauses all token transfers.
+     * @notice Can only be called by the contract owner.
+     */
+    function pause() public {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     * @notice Can only be called by the contract owner.
+     */
+    function unpause() public {
+        _unpause();
+    }
+
+    /**
+     * @dev Allows sending tokens to multiple recipients in a single transaction.
+     * @param recipients An array of recipient addresses.
+     * @param amounts An array of token amounts to send to each recipient.
+     * @notice The length of recipients and amounts arrays must be the same.
+     */
+    function multiSend(address[] memory recipients, uint256[] memory amounts) public {
+        require(recipients.length == amounts.length, "Recipients and amounts length mismatch");
+        
+        for (uint256 i = 0; i < recipients.length; i++) {
+            transfer(recipients[i], amounts[i]);
+        }
+    }
+
+    /**
+     * @dev Returns the maximum supply of tokens.
+     * @return The maximum supply of tokens.
+     */
+    function maxSupply() public view returns (uint256) {
+        return _maxSupply;
+    }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens.
+     * @param from The address tokens are transferred from.
+     * @param to The address tokens are transferred to.
+     * @param amount The amount of tokens transferred.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
